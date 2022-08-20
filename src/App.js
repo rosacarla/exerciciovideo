12c4a6faf2fca457086497784b2b80d0238efe2d
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2022-08-20 00:35:34 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-//criacao de class component similar ao que foi feito com function e array funtion
+//criacao de class component similar ao que foi feito com function e array function
 import React, {Component} from 'react'; //importado o componente Component como boa pratica
 
 //criado componente do tipo classe
 class App extends Component{ //App estende de Component (heranca)
 
-  //render() é metodo obrigatorio em componente do tipo classe; construtor nao é obrigatorio
-  render(){  //nao precisa especificar tipo do metodo como se faz em Java com void etc.
+  //render() é metodo obrigatorio em componente do tipo classe; metodo construtor nao é obrigatorio
+  render(){  //nao precisa especificar tipo do metodo como se faz em Java com void etc...
     return (  //render() deve ter a funcao return para retornar uma html
       <div>
         Olá, Mundo!
