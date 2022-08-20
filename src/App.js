e659d7c13c2cb44f8bb608f0856e53f5c8e79516
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2022-08-20 01:38:45 -0300

    metodo bind()

--- src/App.js
+++ src/App.js
@@ -8,15 +8,24 @@ class App extends Component{ //adiciona metodo construtor para criar estado
 
     this.state = {  //especifica o estado dentro do construtor; por padrao usa-se nomenclatura "state"
       titulo: "Primeira pagina com Class Component"  //atributo do componente com sua variavel
-
     }
+
+    //aplicacao do metodo bind() ao construtor do componente, permite a execucao de mudar()
+
+    this.mudar = this.mudar.bind(this); //executa o bind e traz a referencia para dentro dele
   }
 
   mudar(){  //metodo mudar
-    console.log("mudou"); //mensagem para testar se metodo mudar funcionou
+    
+    //criada variavel para mudar o titulo da pagina
+    var novoTitulo = "Aprendi a utilizar Class Component";
+
+    //executa metodo setState, herdado de React.Component, para modificar estado do componente
+    this.setState({titulo:novoTitulo});  //recarrega estado com variavel ou string e atualiza a tela automaticamente
+
   }
 
-  //criacao de botao para mudar titulo da pagina automaticamente sem recarrega-la (sem F5)
+  //criacao de botao para mudar titulo da pagina automaticamente sem recarrega-la (sem F5), nem usar servidor
   render(){  
     return (  //incluido metodo onClick para executar após click no botao
       <div>
